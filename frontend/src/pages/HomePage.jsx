445fbbbd473362afe4808480e3703f6df733b866
CBBell99 <chrisbeckertonbell@gmail.com>
2022-10-07 03:56:46 -0700

    add simple header, text and button to homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -13,6 +13,18 @@ function HomePage() {
           </div>
         </nav>
       </header>
+      <main>
+        <div className="container mt-3">
+          <div className="bg-light p-5 mt-4 rounded-3">
+            <h1>Welcome to the simple POS for small businesses</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p>If you have an issue, call 443-444-xxx anytime</p>
+            <Link to="/pos" className="btn btn-primary">
+              Click here to sell products
+            </Link>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
