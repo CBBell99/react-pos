bc008131a29060c498549326968c7827b83dbd99
CBBell99 <chrisbeckertonbell@gmail.com>
2022-10-07 08:18:41 -0700

    fetch data from backend and display on frontend

--- frontend/src/pages/POSPage.jsx
+++ frontend/src/pages/POSPage.jsx
@@ -4,17 +4,47 @@ import MainLayout from "../layouts/MainLayout";
 
 function POSPage() {
   const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchProducts = async () => {
+    setIsLoading(true);
     const result = await axios.get("products");
     setProducts(await result.data);
+    setIsLoading(false);
   };
 
   useEffect(() => {
     fetchProducts();
-  });
+  }, []);
 
-  return <MainLayout>POS Content</MainLayout>;
+  return (
+    <MainLayout>
+      <div className="row">
+        <div className="col-lg-8">
+          {isLoading ? (
+            "Loading"
+          ) : (
+            <div className="row">
+              {" "}
+              {products.map((product, key) => (
+                <div key={key} className="col-lg-4">
+                  <div className="border"></div>
+                  <p>{product.name}</p>
+                  <img
+                    src={product.image}
+                    className="img-fluid"
+                    alt={product.name}
+                  />
+                  <p>${product.price}</p>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+        <div className="col-lg-4"></div>
+      </div>
+    </MainLayout>
+  );
 }
 
 export default POSPage;
