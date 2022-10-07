c138e66f32384370ead892909a33f2d1c890dc14
CBBell99 <chrisbeckertonbell@gmail.com>
2022-10-07 07:48:58 -0700

    fetch products from backend using axios

--- frontend/src/pages/POSPage.jsx
+++ frontend/src/pages/POSPage.jsx
@@ -1,9 +1,18 @@
-import React from "react";
-import { useEffect } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import MainLayout from "../layouts/MainLayout";
 
 function POSPage() {
-  useEffect(() => {});
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const result = await axios.get("products");
+    setProducts(await result.data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  });
 
   return <MainLayout>POS Content</MainLayout>;
 }
