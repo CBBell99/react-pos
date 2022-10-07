e015a74e6e932f4b54653282002af731b03c5ee5
CBBell99 <chrisbeckertonbell@gmail.com>
2022-10-07 07:42:52 -0700

    install axios

--- frontend/src/pages/POSPage.jsx
+++ frontend/src/pages/POSPage.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { useEffect } from "react";
 import MainLayout from "../layouts/MainLayout";
 
 function POSPage() {
+  useEffect(() => {});
+
   return <MainLayout>POS Content</MainLayout>;
 }
 
